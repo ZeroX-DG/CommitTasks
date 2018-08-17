cbf0f594d7c24794f4f36ffab6ec83d1abbfdabd
Nguyễn Việt Hưng <hunggarena12@gmail.com>
2018-08-18 00:13:19 +0700

    added add task in manager

--- src/task-manager.js
+++ src/task-manager.js
@@ -9,6 +9,16 @@ const TaskManager = (flags, input) => {
     render.logError(error)
   }
   if (flags.task) {
+    if (!input[0].startsWith('@')) {
+      render.logError({
+        message: 'Wrong add task command syntax. Type --help for more info'
+      })
+      return
+    }
+    const project = input[0]
+    input.shift()
+    const message = input.join(' ')
+    api.addTask(message, project)
   }
   return render.drawTaskList(api.tasks)
 }
