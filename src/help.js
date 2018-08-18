875552287578adcc9356dd3e830a9afc2da3742b
Nguyễn Việt Hưng <hunggarena12@gmail.com>
2018-08-18 15:56:01 +0700

    updated help message

--- src/help.js
+++ src/help.js
@@ -9,7 +9,7 @@ module.exports = `
       --commit, -c      Commit a task
       --list, -l        Display all tasks in a project
       --find, -f        Search for a task
-      --move, -m        Move task between projects or up/down in current project
+      --move, -m        Move task between projects
       --edit, -e        Edit a task commit's message
       --create, -cp     Create project for a task (use along with --task)
 `
