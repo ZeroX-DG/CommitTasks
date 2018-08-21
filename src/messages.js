7e0802cbbbfa75d97bd986f0aa07e3e907f06dcd
Nguyễn Việt Hưng <hunggarena12@gmail.com>
2018-08-21 22:36:19 +0700

    updated messagae

--- src/messages.js
+++ src/messages.js
@@ -1,17 +1,17 @@
 const errors = {
   INVALID_PROJECT_NAME: 'There are no projects named %s',
   INVALID_TASK_ID_TYPE: 'Task id is not a number',
-  NO_TASK_IN_PROJECT: 'There is no task with the id %s in @%s',
+  NO_TASK_IN_PROJECT: 'There is no task with the id %s in %s',
   COMMIT_FINISHED_TASK: "You can't commit a finished task",
   NOTHING_TO_COMMIT: 'No changes to commit!'
 }
 
 const success = {
-  REMOVED_TASK: 'Removed task %s in @%s',
+  REMOVED_TASK: 'Removed task %s in %s',
   CREATED_TASK: 'Created task %s',
-  COMITTED_TASK: 'Commited task %s in @%s',
+  COMITTED_TASK: 'Commited task %s in %s',
   EDITTED_TASK: 'Changed message to -> %s',
-  REMOVED_PROJECT: 'Removed project @%s'
+  REMOVED_PROJECT: 'Removed project %s'
 }
 
 function getMessage (message, data) {
